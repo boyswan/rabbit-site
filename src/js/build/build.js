430702590f1488feeac4d840b868df0e461215d6
boyswan <jackwilliamboyce@gmail.com>
2014-12-15 12:49:56 +0000

    responsive fix, icons

--- src/js/build/build.js
+++ src/js/build/build.js
@@ -1,13 +1,11 @@
-
   $(window).on('resize', function(){
 
-		if ($(window).width() <= 700){	
+		if ($(window).width() <= 790){	
 			$('#secondary__content').prependTo('#secondary__row');
 		}
 
-		if ($(window).width() > 700){	
+		if ($(window).width() > 790){	
 			$('#secondary__content').appendTo('#secondary__row');
 		}
   }).trigger('resize');
-
 
